ea3436ab9db173456d9abc5ba59f6dcf7994eca4
François Beausoleil <francois@teksol.info>
2010-04-28 14:22:20 -0400

    Basic plugin

---
+++ adgear-ad-manager.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Plugin Name: AdGear WP Plugin
+ * Plugin URI: http://github.com/bloom/adgear-wp-plugin
+ * Description: Server AdGear ads through your blog
+ * Version: 0.1-alpha
+ * Author: Bloom Digital Platforms
+ * Author URI: http://github.com/bloom/adgear-wp-plugin
+ * License: GPL2
+ * */
+?>
+<?php
+/*  Copyright 2010  Bloom Digital Platforms  ( adgear-wp@bloomdigital.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
