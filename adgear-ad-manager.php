7a52de250cae9d54e7ffd97d300b66a7c2f0d990
François Beausoleil <francois@teksol.info>
2010-04-28 15:14:31 -0400

    Let admins update settings.

--- adgear-ad-manager.php
+++ adgear-ad-manager.php
@@ -26,3 +26,60 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 ?>
+<?php
+error_reporting( E_ALL );
+
+adgear_init();
+
+function adgear_init() {
+	define( 'ADGEAR_PATH', dirname( __FILE__ ) );
+	define( 'ADGEAR_URL', get_bloginfo( 'wpurl' ) . '/wp-content/plugins/adgear-wp-plugin' );
+
+  if ( is_admin() ) {
+    add_action('admin_menu', 'adgear_create_menu');
+  }
+}
+
+function adgear_create_menu() {
+  add_submenu_page( 'options-general.php', 'AdGear Settings', 'AdGear Settings', 'administrator', __FILE__, 'adgear_settings_page' );
+  add_action( 'admin_init', 'adgear_register_settings' );
+}
+
+
+function adgear_register_settings() {
+  register_setting( 'adgear-settings-group', 'api_username' );
+  register_setting( 'adgear-settings-group', 'api_key' );
+  register_setting( 'adgear-settings-group', 'api_root_url' );
+}
+
+function adgear_settings_page() {
+?>
+<div class="wrap">
+<h2>AdGear Settings</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'adgear-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">API Username</th>
+        <td><input type="text" name="api_username" value="<?php echo get_option('api_username', ''); ?>" /></td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">API Digest Key</th>
+        <td><input type="text" name="api_key" size="68" value="<?php echo get_option('api_key', ''); ?>" /></td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">API Root URL</th>
+        <td><input type="text" name="api_root_url" size="40" value="<?php echo get_option('api_root_url', 'http://api.adming.adgear.com/'); ?>" /></td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+</form>
+</div>
+<?php } ?>
